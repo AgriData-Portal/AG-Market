4eac73238d83351788526a85cee5738bfa4bdcd8
jollyjunkaih <jwong091@e.ntu.edu.sg>
2021-07-23 01:27:33 +0800

    renamed app to Market

--- ios/MarketTests/MarketTests.m
+++ ios/MarketTests/MarketTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AgriPasarTests : XCTestCase
+@interface MarketTests : XCTestCase
 
 @end
 
-@implementation AgriPasarTests
+@implementation MarketTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
